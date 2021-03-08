97eb4541849a4d58e092e5e5cd4f559f82cedcd1
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2021-03-08 17:09:41 -0300

    Bug fixes.

--- bin/install.sh
+++ bin/install.sh
@@ -8,7 +8,7 @@ sed -i -e 's|${LOG_DIR}|'"$LOG_DIR"'|g' /tmp/my.cnf
 cp /tmp/my.cnf /etc/my.cnf
 
 cp $ETC_DIR/flyway.conf /tmp/flyway.conf
-sed -i -e 's|${$DATABASE_NAME}|'"$DATABASE_NAME"'|g' /tmp/flyway.conf
+sed -i -e 's|${DATABASE_NAME}|'"$DATABASE_NAME"'|g' /tmp/flyway.conf
 sed -i -e 's|${USER}|'"$USER"'|g' /tmp/flyway.conf
 sed -i -e 's|${PORT}|'"$PORT"'|g' /tmp/flyway.conf
 cp /tmp/flyway.conf /opt/flyway/conf
